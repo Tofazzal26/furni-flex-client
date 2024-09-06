c9f18632c9bfe3a5f7e481c8f423c86285796aaa
Tofazzal26 <afranislamabir6789@gmail.com>
2024-09-06 15:37:59 +0600

    axios and tanstack quary validation finish

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,20 @@ import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router.jsx";
 import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={Router}></RouterProvider>
-      <Toaster />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={Router}></RouterProvider>
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
